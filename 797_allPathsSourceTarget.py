98e71332bdc61053d2671804ceb6354ffc1fcf98
zhouzhiz <zzz686970@gmail.com>
2018-07-25 01:35:23 +0800

    dfs algorithm

--- 797_allPathsSourceTarget.py
+++ 797_allPathsSourceTarget.py
@@ -1,14 +1,33 @@
 def allPathsSourceTarget(graph):
 	result = []
-	def dfs(x, path):
-		if x == len(graph) - 1:
-			result.append(path)
-			for z in graph[x]:
-				dfs(z, path+[z])
-			dfs(0, [0])
+	for i in graph:
+		i.sort()
+	path = [0]
+	def dfs(n, path):
+		for i in graph[n]:
+			# print("-----")
+			# print(i, path)
+			# print("*******")
+			path.append(i)
+			if i == len(graph) - 1:
+				result.append(path[:])
+				path.pop()
+				if i == graph[n][-1]:
+					path.pop()
+					# print(path)
+				return
+
+			else:
+				dfs(i, path)
+				if i == graph[n][-1]:
+					path.pop()
+					# print("^^^^^^^")
+					# print(path)
+					# print("&&&&&&&&&")
+	dfs(0, path)
 
 	return result
 
-graph = [[1,2], [3], [3], []] 
+graph = [[2,1], [3], [3], []] 
 print(allPathsSourceTarget(graph))
 
