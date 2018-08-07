4f53a0e22e21ed29c35eaf3a972fd7d9a1fea0ca
zhouzhiz <zzz686970@gmail.com>
2018-08-07 15:49:16 +0800

    163 email login

--- other/write_email.py
+++ other/write_email.py
@@ -1,22 +1,30 @@
 import smtplib
+from email import encoders
 from email.mime.text import MIMEText
 from email.header import Header
+from email.utils import parseaddr, formataddr
+
+def _format_addr(s):
+	name, addr = parseaddr(s)
+	return formataddr((Header(name, 'utf-8').encode(), addr))
 
 mail_user = 'zzz686970@163.com'
-mail_pass = 'Ilovemyself520'
+mail_pass = 'Ilove1025'
 
 msg = MIMEText("测试邮件，需要进一步完善", 'plain', 'utf-8')
 
 msg['Subject'] = Header("Test", 'utf-8')
-msg['From'] = "zzz686970@163.com"
-msg['To'] = 'e0146282@u.nus.edu'
+msg['From'] = _format_addr('匿名用户 <%s>' % "zzz686970@163.com")
+msg['To'] = _format_addr('Admin <%s>' % "e0146282@u.nus.edu") 
 
 try:
-	server = smtplib.SMTP()
-	server.connect('smtp.163.com', 465)
-	server.login("zzz686970", mail_pass)
-	server.sendmail(mail_user ,'1564256031@qq.com', msg.as_string())
-	server.close()
+	# server = smtplib.SMTP()
+	# server.connect('smtp.163.com', 465)
+	server = smtplib.SMTP("smtp.163.com", 25)
+	# server.set_debuglevel(1)
+	server.login(mail_user, mail_pass)
+	server.sendmail(mail_user ,['1564256031@qq.com'], msg.as_string())
+	server.quit()
 	print("Send mail successfully")
 except smtplib.SMTPException as e:
 	print(str(e))
