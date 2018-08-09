a079dd7a3f69ca8d936f5132826ec3377fb89c4d
zzz686970 <1564256031@qq.com>
2018-08-09 21:14:57 +0800

    change username, bitwise

---
+++ 136_singleNumber.py
@@ -0,0 +1,14 @@
+def singleNumber(nums):
+	# result = [nums.count(ele) for ele in nums]
+	# return nums[result.index(1)]
+	# for ele in nums:
+	# 	if nums.count(ele) ==1:
+	# 		return ele
+	result =  0
+	for ele in nums:
+		result ^= ele
+
+	return result
+
+nums = [4,1,2,1,2]
+print(singleNumber(nums))
