3799bd98e32b7d9aca330ea11f0fc2486acffc91
zzz686970 <1564256031@qq.com>
2018-08-10 20:26:15 +0800

    move zeroes

---
+++ 283_moveZeroes.py
@@ -0,0 +1,38 @@
+def moveZeroes(nums):
+	# index = []
+	# for i, value in enumerate(nums):
+	# 	if nums[i] == 0:
+	# 		index.append(i)
+
+	# for i in index:
+	# 	del nums[i]
+
+	# 	nums.append(0)
+	# return nums
+	i = 0
+	for num in nums:
+		if num != 0:
+			nums[i] = num
+			i += 1
+		for j in range(i, len(nums)):
+			nums[j] = 0
+
+	return nums
+
+	slow = fast = 0
+	while fast < len(nums):
+		if nums[fast] !=0:
+			if slow != fast:
+				nums[slow] = nums[fast]
+				nums[fast] = 0
+			slow += 1
+
+		fast += 1
+
+	## return nums.sort(key = lambda x: 1 if x == 0 else 0)
+
+
+
+
+nums = [0,1,0,3,12]
+print(moveZeroes(nums))
