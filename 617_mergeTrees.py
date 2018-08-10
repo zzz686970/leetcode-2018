0f32f130b379652865412ed18a8a491ecb15c159
zzz686970 <1564256031@qq.com>
2018-08-10 20:00:37 +0800

    merge tree

---
+++ 617_mergeTrees.py
@@ -0,0 +1,11 @@
+def mergeTrees(t1, t2):
+	if t1 is None:
+		return t2
+	if t2 is None:
+		return t1
+
+	t1.val += t2.val
+	t1.left = self.mergeTrees(t1.left, t2.left)
+	t1.right = self.mergeTrees(t1.right, t2.right)
+
+	return t1
