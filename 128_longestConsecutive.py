d36b1e8e0c324916b008a8ee339d44561b2daceb
zzz686970 <1564256031@qq.com>
2018-08-14 18:36:01 +0800

    modify longestconsecutives

--- 128_longestConsecutive.py
+++ 128_longestConsecutive.py
@@ -1,14 +1,14 @@
 def longestConsecutive(nums):
 	init = {x: False for x in nums}
-	result = 1
+	result = 0
 	for i in nums:
 		if init[i] is False:
 			init[i] = True
 			left, right = init.get(i-1, 0), init.get(i+1, 0)
 			length = 1+ left + right
-			result, init[i-left], init[i+left] = max(result, length), length, length
+			result, init[i-left], init[i+right] = max(result, length), length, length
 
 	return result
 
-nums = [100, 4, 200, 1, 3, 2]
+nums = [0,3,7,2,5,8,4,6,0,1]
 print(longestConsecutive(nums))
