382eb3a561acc4015f0c8e36025fa423588b4fe7
zzz686970 <1564256031@qq.com>
2018-08-14 18:31:10 +0800

    maxsubarray

--- 53_maxSubArray.py
+++ 53_maxSubArray.py
@@ -1,7 +1,12 @@
 def maxSubArray(nums):
-	init = {x: False for x in nums}
+	newsum=nums[0]
+	max_out=nums[0]
+	for i in range(1,len(nums)):
+		newsum = max(newsum+nums[i], nums[i])
+		max_out = max(newsum, max_out)
+		# print(i, newsum, max_out)
 
-	print(init)
+	return max_out
 
-nums = [100, 4, 200, 1, 3, 2]
-print(maxSubArray)
+nums=[-2,1,-3,4,-1,2,1,-5,4]
+print(maxSubArray(nums))
