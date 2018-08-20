8f30fc8c9a8d855b39d115eae55175b3a155fa08
zzz686970 <1564256031@qq.com>
2018-08-21 03:09:34 +0800

    split word to list

--- 434_countSegments.py
+++ 434_countSegments.py
@@ -1,15 +1,20 @@
 def countSegments(s):
-	result = []
-	cur = 0
-	if s == "":
+	result = [char for char in s.split(" ") if char]
+	if result == []:
 		return 0
-	for index, char in enumerate(s):
-		if s[index] == " " :
-			result.append(s[cur:index])
-			cur = index + 1
-	if s[cur:] != " ":
-		result.append(s[cur:])
-	return result
+	else:
+		return len(result)
+	# result = []
+	# cur = 0
+	# if s == "":
+	# 	return 0
+	# for index, char in enumerate(s):
+	# 	if s[index] == " " :
+	# 		result.append(s[cur:index])
+	# 		cur = index + 1
+	# if s[cur:] != " ":
+	# 	result.append(s[cur:])
+	# return result
 
 s = "                "
 print(countSegments(s))
