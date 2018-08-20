6a5d2ac015141aa20515adda949ec462a492f3ca
zzz686970 <1564256031@qq.com>
2018-08-20 18:42:31 +0800

    non-decreasing

--- 665_checkPossibility.py
+++ 665_checkPossibility.py
@@ -1,19 +1,36 @@
 def checkPossibility(nums):
+	if len(nums) < 2:
+		return True
+	
 	cur = 0
 
-	for i in range(len(nums)-1):
-		if nums[i] > nums[i+1]:
-			cur = i+1
-			break
-	newlist = nums[:cur] + nums[cur+1:]
-	print(newlist)
-	for i in range(len(newlist) -1):
-		if newlist[i] > newlist[i+1]:
-			return False
-
+	for i in range(1, len(nums)):
+		if nums[i-1] > nums[i]:
+			cur += 1
+			if i == 1 or nums[i-2] <= nums[i]:
+				nums[i-1] = nums[i]
+			else:
+				nums[i] = nums[i-1]
+			if cur > 1:
+				return False
+	print(nums)
 	return True
 
 
-nums = [2,3,3,2,4]
+	## 108ms
+	# if len(nums)<2:
+	# return True
+	# a1=nums[:]
+	# a2=nums[:]
+	# for i in range(len(nums)-1):
+	# 	if nums[i]>nums[i+1]:
+	# 		a1[i]=nums[i+1]
+	# 		a2[i+1]=nums[i]
+	# 		break
+	# return a1==sorted(a1) or a2==sorted(a2)
+
+
+
+nums = [-1,4,2,3]
 print(checkPossibility(nums))
 
