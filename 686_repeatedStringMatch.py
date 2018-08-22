6b556de602129c1a4f4af86706ba9ebfa42475df
zzz686970 <1564256031@qq.com>
2018-08-23 00:55:44 +0800

    repeated string

--- 686_repeatedStringMatch.py
+++ 686_repeatedStringMatch.py
@@ -1,13 +1,13 @@
 def repeatedStringMatch(A, B):
-	for i in range(len(B)-2):
-		if B[i:i+2] not in A*2:
-			return -1
-	for i in range(1, 10001):
-		if B in A*i:
+	start = int(len(B) / len(A))
+	# for i in range(len(B)-2):
+	# 	if B[i:i+2] not in A*2:
+	# 		return -1
+	for i in range(start, start+3):
+		if len(A*i) > len(B) and  B in A*i:
 			return i
-			break
 	return -1
 
-A = "abcd" 
-B = "cdabcdab"
+A = "baaaabbbba"
+B = "bbaaaabbbbaabaaaabbbbaabaaaabbbbaabaaaabbbbaabaaaabbbbabbaaaabbbbabbaaaabbbbabbaaaabbbbabbaaaabbbbaa"
 print(repeatedStringMatch(A, B))
