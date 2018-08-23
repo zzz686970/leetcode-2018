b5037ac1b970e9d358499ba1ecfe9953d3740e62
zzz686970 <1564256031@qq.com>
2018-08-23 22:05:21 +0800

    countAndSay

--- 459_repeatedSubstringPattern.py
+++ 459_repeatedSubstringPattern.py
@@ -1,11 +1,27 @@
 def repeatedSubstringPattern(s):
-	## can't ac
-	for i in range(1, int(len(s)/2) + 1):
-		if s.count(s[:i]) == len(s)/len(s[:i]):
-			return True
+	## ac but slow
+	# for i in range(1, int(len(s)/2) + 1):
+	# 	if len(s) % i:
+	# 		continue
+	# 	if s[:i] * int(len(s) / i) == s:
+	# 		return True
 
-	return False
+	# return False
+	size = len(s)
+	next = [0] * size
+	for i in range(1, size):
+		k = next[i-1]
+		print(next)
+		while s[i] != s[k] and k:
+			k = next[k-1]
+
+		if s[i] == s[k]:
+			next[i] = k+1
+
+	p = next[-1]
 
 
-s = 'aba'
+	return p>0 and size % (size-p) == 0
+
+s = 'abcdabcabcdabcdc'
 print(repeatedSubstringPattern(s))
