ce42130ac6fffb9dbe7d13720fb23d98f25aacc9
zzz686970 <1564256031@qq.com>
2018-08-24 22:46:58 +0800

    add without + and -

--- 371_getSum.py
+++ 371_getSum.py
@@ -1,4 +1,9 @@
 def getSum(a, b):
+	## way1
+	# return sum([a,b])
+
+	## way2
+	## reference https://leetcode.com/problems/sum-of-two-integers/discuss/132479/Simple-explanation-on-how-to-arrive-at-the-solution/168383
 	while  b != 0:
 		carry = a & b
 		a = (a ^ b) % 0x100000000
