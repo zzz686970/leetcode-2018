75ce8e3d569901d9b4d1a5dc3ef28ad66ac98f63
zzz686970 <1564256031@qq.com>
2018-08-26 02:52:28 +0800

    missed value by comparing the length

--- 448_findDisappearedNumbers.py
+++ 448_findDisappearedNumbers.py
@@ -1,12 +1,24 @@
 def findDisappearedNumbers(nums):
-	max_number = max(nums)
-	min_number = min(nums)
-	ans = []
-	for i in range(min_number, max_number+1):
-		if i not in nums:
-			ans.append(i)
+	## can't ac
+	# if nums == []:
+	# 	return []
+	# n = len(nums)
+	# # max_number = max(nums)
+	# # min_number = min(nums)
+	# ans = []
+	# for i in range(1, n+1):
+	# 	if i not in nums:
+	# 		ans.append(i)
 
-	return ans
+	# return ans
+
+	n = len(nums)
+	if n == 0: return []
+	for i in range(n):
+		num = nums[i]
+		while num != -1:
+			nums[num-1], num = -1, nums[num-1]
+	return [i+1 for i in range(n) if nums[i] !=-1]
 
 nums = [4,3,2,7,8,2,3,1]
 print(findDisappearedNumbers(nums))
