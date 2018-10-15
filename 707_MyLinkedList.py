f0402404623c7ca9140648bb2e5f0ca5d91831d3
zzz686970 <1564256031@qq.com>
2018-10-15 19:23:58 +0800

    design a linked list

--- 707_MyLinkedList.py
+++ 707_MyLinkedList.py
@@ -1,14 +1,18 @@
+class Node(object):
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
 class MyLinkedList(object):
 
     def __init__(self):
         """
         Initialize your data structure here.
-        isnil: check whether the index exists
         """
-        self.isnil = True
-        self.val = None
-        self.next = None
-        
+        self.head = None
+        self.size = 0
 
     def get(self, index):
         """
@@ -16,32 +20,29 @@ class MyLinkedList(object):
         :type index: int
         :rtype: int
         """
-        if self.isnil:
+        if index < 0 or index >= self.size:
             return -1
-        elif index == 0:
-            return self.val
-        else:
-            return self.next.get(index-1)
-        
+
+        if self.head is None:
+            return -1
+
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr.val
 
     def addAtHead(self, val):
         """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        Add a node of value val before the first element of the linked list.
+        After the insertion, the new node will be the first node of the linked list.
         :type val: int
         :rtype: void
         """
-        if self.isnil:
-            self.val = val
-            self.next = MyLinkedList()
-            self.isnil = False
-        else:
-            new = MyLinkedList()
-            new.isnil = False
-            new.val = self.val
-            new.next = self.next
-            self.val = val
-            self.new = new
-        
+        node = Node(val)
+        node.next = self.head
+        self.head = node
+
+        self.size += 1
 
     def addAtTail(self, val):
         """
@@ -49,32 +50,39 @@ class MyLinkedList(object):
         :type val: int
         :rtype: void
         """
-        if self.isnil:
-            self.addAtHead(val)
+        curr = self.head
+        if curr is None:
+            self.head = Node(val)
         else:
-            self.next.addAtTail(val)
-        
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = Node(val)
+
+        self.size += 1
 
     def addAtIndex(self, index, val):
         """
-        Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+        Add a node of value val before the index-th node in the linked list.
+        If index equals to the length of linked list, the node will be appended to the end of linked list.
+        If index is greater than the length, the node will not be inserted.
         :type index: int
         :type val: int
         :rtype: void
         """
-        pass
-        # if self.isnil  and index > 0:
-        #     return
-        # elif index == 0:
-        #     if self.isnil:
-        #         self.next = None
-        #         self.isnil = True
-        #     else:
-        #         self.next.val = self.val
-        #         self.next.next = self.next
-        # else:
-        #     self.next.addAtIndex(index-1, val)
-        
+        if index < 0 or index > self.size:
+            return
+
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            curr = self.head
+            for i in range(index - 1):
+                curr = curr.next
+            node = Node(val)
+            node.next = curr.next
+            curr.next = node
+
+            self.size += 1
 
     def deleteAtIndex(self, index):
         """
@@ -82,18 +90,19 @@ class MyLinkedList(object):
         :type index: int
         :rtype: void
         """
-        if self.isnil:
+        if index < 0 or index >= self.size:
             return
-        elif index == 0:
-            if self.next.isnil:
-                self.next = None
-                self.isnil = True
-            else:
-                self.val = self.next.val
-                self.next = self.next.next
+
+        curr = self.head
+        if index == 0:
+            self.head = curr.next
         else:
-            self.next.deleteAtIndex(index - 1)
-        
+            for i in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+
+        self.size -= 1
+
 
 
 # Your MyLinkedList object will be instantiated and called as such:
