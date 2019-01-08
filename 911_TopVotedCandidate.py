398fd04d8551f7039f1c07758aba7fc607d784a8
zzz686970 <1564256031@qq.com>
2019-01-09 01:15:10 +0800

    bisect pos

---
+++ 911_TopVotedCandidate.py
@@ -0,0 +1,27 @@
+class TopVotedCandidate:
+
+	def __init__(self, persons, times):
+		"""
+		:type persons: List[int]
+		:type times: List[int]
+		"""
+		self.res, self.times = [], times 
+		cnt, win = {}, -1
+		for t, p in zip(times,persons):
+			cnt[p] = cnt.get(p, 0) + 1
+			if cnt[p] >= cnt.get(win, 0): win = p
+			self.res.append(win)
+
+
+
+	def q(self, t):
+		"""
+		:type t: int
+		:rtype: int
+		"""
+		return self.res[bisect.bisect(self.times, t)-1]
+
+
+# Your TopVotedCandidate object will be instantiated and called as such:
+# obj = TopVotedCandidate(persons, times)
+# param_1 = obj.q(t)
