f7eada8ac2c982bd7cd28ddbf992914ef406c2d7
zzz686970 <1564256031@qq.com>
2019-01-21 23:53:31 +0800

    dp search to find smallest sum-up

--- 64_minPathSum.py
+++ 64_minPathSum.py
@@ -1,13 +1,21 @@
 def minPathSum(grid):
 	m, n = len(grid), len(grid[0])
 	if m < 2 or n < 2: return sum([sum(i) for i in grid])
-	for i in range(1, m):
-		grid[i][0] += grid[i-1][0]
-	for i in range(1, n):
-		grid[0][i] += grid[0][i-1]
+	# for i in range(1, m):
+	# 	grid[i][0] += grid[i-1][0]
+	# for i in range(1, n):
+	# 	grid[0][i] += grid[0][i-1]
 
 	for i in range(1, m):
+		grid[i][0] += grid[i-1][0]
 		for j in range(1, n):
+			grid[0][j] += grid[0][j-1]
 			grid[i][j] += grid[i-1][j] if grid[i-1][j] < grid[i][j-1] else grid[i][j-1]
 
 	return grid[-1][-1]
+
+print(minPathSum([
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]))
