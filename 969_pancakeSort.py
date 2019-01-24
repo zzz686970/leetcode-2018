eb6e13d613143793688162385ae467e4edfa3169
zzz686970 <1564256031@qq.com>
2019-01-24 09:23:25 +0800

    pancake sort general

--- 969_pancakeSort.py
+++ 969_pancakeSort.py
@@ -1,9 +1,16 @@
 def pancakeSort(A):
-	## find index 
+	## find index of the next maximum number
+	## reverse i+1 numbbers, x will be A[0], insert this to moves
+	## reverse x numbers, so x will be A[x-1]
+	## repeat N times
+	## if permutation is not [1,2,..., A.length], then sorted first 
 	moves = []
-	for x in range(len(A),1, -1):
-		i = A.index(x)
-		moves.extend([i+1, x])
-		A = A[:i:-1] + A[:i]
+	# for x in range(len(A),1, -1):
+	for x in sorted(A)[::-1]:
+		# i = A.index(x)
+		moves.extend([idx+1, x])
+
+		## reverse idx..n numbers, 
+		A = A[:idx:-1] + A[:idx]
 
 	return moves 
