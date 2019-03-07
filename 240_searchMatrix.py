5b052a9fe9cd799ca86b162b31cb4abb5c955fd8
zzz686970 <1564256031@qq.com>
2019-03-07 22:18:25 +0800

    search 2d matrix

--- 240_searchMatrix.py
+++ 240_searchMatrix.py
@@ -10,3 +10,24 @@ def searchMatrix(matrix, target):
 			return True 
 
 	return False 
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        if not matrix: return False
+        for row in matrix:
+            if self.search(row, target):
+                return True
+        
+        return False
+    
+    def search(self, row, target):
+        l, r = 0, len(row) - 1
+        while l <= r:
+            mid = (l+r) // 2
+            if row[mid] == target:
+                return True 
+            elif row[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
