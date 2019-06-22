058104e51bf15b46bbf4e92686f9b1d8ad83ea94
zzz686970 <1564256031@qq.com>
2019-06-22 20:57:08 +0800

    triangle

--- 1037_isBoomerang.py
+++ 1037_isBoomerang.py
@@ -1,5 +1,9 @@
 def isBoomerang(points):
-	pass
+	a, b, c = points
+	if (a[0] - b[0]) *  (a[1]-c[1]) ==  (a[1]-b[1]) * (a[0] - c[0]) :
+		return False 
 
-def get_first_name(full_name: str) -> str:
-	pass
+	return True
+
+print(isBoomerang([[1,1],[2,3],[3,2]]))
+print(isBoomerang([[1,1],[2,2],[3,3]]))
