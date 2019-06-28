cc65fd11b14582c50592bc9dc3ae52a0e0c95131
zzz686970 <1564256031@qq.com>
2019-06-28 09:47:41 +0800

    gcd

--- 1071_gcdOfStrings.py
+++ 1071_gcdOfStrings.py
@@ -1,8 +1,11 @@
 def gcdOfStrings(str1, str2):
-	ans = ''
+	if len(str1) == len(str2):
+		return str1 if str1 == str2 else ''
 	a_str, b_str = sorted([str1, str2], key = len)
-	for el in b_str:
-		if el in a_str:
-			ans += el
-		else:
+	if b_str[:len(a_str)] == a_str:
+		return gcdOfStrings(b_str[len(a_str):], a_str)
+	else:
+		return ''
+
+print(gcdOfStrings(str1 = "ABABAB", str2 = "ABAB"))
 
