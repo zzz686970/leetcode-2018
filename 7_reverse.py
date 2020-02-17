d61cfad370ce735c6835e88308521b565296ebb7
zzz686970 <1564256031@qq.com>
2020-02-17 12:14:19 +0800

    code update2

--- 7_reverse.py
+++ 7_reverse.py
@@ -5,16 +5,21 @@ def reverse( x):
     """
     c = 0
     if x<0 and x> -(2 ** 31):
-        c = int("-"+"".join(char for char in reversed(str(-x)))) 
+        c = int("-"+"".join(char for char in reversed(str(-x))))
         return c if c > -(2 ** 31) else 0
-        
+
     elif x >= 0 and x < 2 ** 31 - 1:
-        c =  int("".join(char for char in reversed(str(x)))) 
+        c =  int("".join(char for char in reversed(str(x))))
         return c if c < 2 ** 31 -1 else 0
     else:
         return 0
 
-print(reverse(1534236469))
+def reverse2(x):
+    ans = int(str(x)[::-1]) if all([x>=0, x< 2 ** 31-1]) else (int("-" + str(x)[:0:-1])
+        if all([x<0 , x> -(2 ** 31)])
+        else 0)
+    return ans if all([ans>=0, ans< 2 ** 31-1]) or all([ans<0 , ans> -(2 ** 31)]) else 0
+print(reverse2(1534236469))
 
 # 900000
 
